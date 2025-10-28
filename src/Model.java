d1c38680288a0113408457905cd98c36c6ab7b8a
Kuba Tuta <tuta.jakub@gmail.com>
2025-10-28 18:31:57 +0100

    delete topButton strings ane active button as an enum

--- src/Model.java
+++ src/Model.java
@@ -13,64 +13,49 @@ public class Model {
     private final StringProperty promptLabelText = new SimpleStringProperty("");
     private final StringProperty inputData = new SimpleStringProperty("");
     private final ListProperty<Node> outputNodes = new SimpleListProperty<>(FXCollections.observableArrayList());
-    private String activeButton = "";
-
-    private final String reg = "Nr rej. / VIN";
-    private final String regContent = "Wpisz nr rej. lub VIN, aby wyświetlić parametry pojazdu";
-    private final String comments = "Komentarze";
-    private final String commentsContent = "Wpisz datę, aby wyświetlić pojazdy z komentarzami z tego dnia (dd.mm.rrrr)";
-    private final String invoiceNumbers = "Faktury danego dnia";
-    private final String invoiceNumbersContent = "Wpisz datę, aby wyświetlić listę faktur z tego dnia (dd.mm.rrrr)";
+    private TopButtonType activeButton;
 
     public ObservableList<Car> getCars() {
         return cars;
     }
+
     public void setCars(List<Car> newCars) {
         this.cars.addAll(newCars);
     }
-    public String getActiveButton() {
+
+    public TopButtonType getActiveButton() {
         return activeButton;
     }
-    public void setActiveButton(String activeButton) {
+
+    public void setActiveButton(TopButtonType activeButton) {
         this.activeButton = activeButton;
     }
+
     public String getPromptLabelText() {
         return promptLabelText.get();
     }
+
     public void setPromptLabelText(String promptLabelText) {
         this.promptLabelText.set(promptLabelText);
     }
+
     public StringProperty getPromptLabelTextProperty() {
         return promptLabelText;
     }
+
     public String getInputData() {
         return inputData.get();
     }
+
     public StringProperty getInputDataProperty() {
         return inputData;
     }
+
     public ObservableList<Node> getOutputNodes() {
         return outputNodes.get();
     }
+
     public void setOutputNodes(List<Node> newOutputNodes) {
         this.outputNodes.setAll(newOutputNodes);
-    }
-    public String getReg() {
-        return reg;
-    }
-    public String getRegContent() {
-        return regContent;
-    }
-    public String getComments() {
-        return comments;
-    }
-    public String getCommentsContent() {
-        return commentsContent;
-    }
-    public String getInvoiceNumbers() {
-        return invoiceNumbers;
-    }
-    public String getInvoiceNumbersContent() {
-        return invoiceNumbersContent;
     }
 }

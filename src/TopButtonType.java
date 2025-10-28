06653fe23501debbb994f3059e134981fe9e6237
Kuba Tuta <tuta.jakub@gmail.com>
2025-10-28 18:26:26 +0100

    enum class for top buttons

---
+++ src/TopButtonType.java
@@ -0,0 +1,21 @@
+public enum TopButtonType {
+        REG("Nr rej. / VIN", "Wpisz nr rej. lub VIN, aby wyświetlić parametry pojazdu"),
+        COMMENTS("Komentarze", "Wpisz datę, aby wyświetlić pojazdy z komentarzami z tego dnia (dd.mm.rrrr)"),
+        INVOICES("Faktury danego dnia", "Wpisz datę, aby wyświetlić listę faktur z tego dnia (dd.mm.rrrr)");
+
+        private final String title;
+        private final String label;
+
+        TopButtonType(String title, String label) {
+            this.title = title;
+            this.label = label;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
